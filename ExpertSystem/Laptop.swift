89640c79059db97e650fdd9eb3146aae7955e9aa
IceRedline <scpd2604@gmail.com>
2025-03-25 13:44:13 +0300

    Изменена логика, добавлены вероятности

---
+++ ExpertSystem/Laptop.swift
@@ -0,0 +1,14 @@
+//
+//  Laptop.swift
+//  ExpertSystem
+//
+//  Created by Артем Табенский on 25.03.2025.
+//
+
+import Foundation
+
+struct Laptop {
+    var name: String
+    var probability: Double
+}
+
--- ExpertSystem/LaptopExpertSystemPresenter.swift
+++ ExpertSystem/LaptopExpertSystemPresenter.swift
@@ -7,17 +7,10 @@
 
 import UIKit
 
-enum Budget {
-    case small
-    case medium
-    case large
-}
-
 class LaptopExpertSystemPresenter {
     
     weak var view: LaptopExpertSystemViewControllerProtocol? // Используем weak, чтобы избежать циклов удержания
     
-    var budget: Budget?
     var recommendation = ""
     private var currentQuestionIndex: Int = 0
     private var questionsAndAnswers: [(String, [String])] = [
@@ -26,6 +19,20 @@ class LaptopExpertSystemPresenter {
         ("Собираетесь ли вы работать подолгу от батареи?", ["Да", "Нет", "Не знаю"]),
         ("Какой размер экрана предпочитаете?", ["До 14 дюймов", "15-16 дюймов", "Более 16 дюймов"])
     ]
+    
+    var asusVivobook13: Laptop = Laptop(name: "Asus VivoBook 13", probability: 0.5)
+    var asusVivobook14: Laptop = Laptop(name: "Asus VivoBook 14", probability: 0.5)
+    var asusVivobook16: Laptop = Laptop(name: "Asus VivoBook 16", probability: 0.5)
+    var asusZenBook14: Laptop = Laptop(name: "Asus ZenBook 14", probability: 0.5)
+    var asusZenBook15: Laptop = Laptop(name: "Asus ZenBook 15", probability: 0.5)
+    var asusZenBookS16: Laptop = Laptop(name: "Asus ZenBook S16", probability: 0.5)
+    var MacBookAir13: Laptop = Laptop(name: "MacBook Air 13", probability: 0.5)
+    var MacBookAir15: Laptop = Laptop(name: "MacBook Air 15", probability: 0.5)
+    var MacBookPro14: Laptop = Laptop(name: "MacBook Pro 14", probability: 0.5)
+    var MacBookPro16: Laptop = Laptop(name: "MacBook Pro 16", probability: 0.5)
+    var DellXPS13: Laptop = Laptop(name: "Dell XPS 13", probability: 0.5)
+    var DellXPS15: Laptop = Laptop(name: "Dell XPS 15", probability: 0.5)
+    var DellXPS17: Laptop = Laptop(name: "Dell XPS 17", probability: 0.5)
     
     init(view: LaptopExpertSystemViewControllerProtocol) {
         self.view = view
@@ -63,88 +70,177 @@ class LaptopExpertSystemPresenter {
         case 0:
             switch answerNumber {
             case 1:
-                budget = .small
-                recommendation += "Учитвая ваш бюджет, вам подойдет ограниченное кол-во бюджетных ноутбуков. "
+                asusVivobook13.probability += 0.2
+                asusVivobook14.probability += 0.2
+                asusVivobook16.probability += 0.2
+                asusZenBook14.probability -= 0.1
+                asusZenBook15.probability -= 10
+                asusZenBookS16.probability -= 10
+                MacBookAir13.probability -= 10
+                MacBookAir15.probability -= 10
+                MacBookPro14.probability -= 10
+                MacBookPro16.probability -= 10
+                DellXPS13.probability -= 10
+                DellXPS15.probability -= 10
+                DellXPS17.probability -= 10
             case 2:
-                budget = .medium
-                recommendation += "Учитвая ваш бюджет, вам подойдет подавляющее большиснтво ноутбуков. "
+                asusVivobook13.probability += 0.1
+                asusVivobook14.probability += 0.1
+                asusVivobook16.probability += 0.2
+                asusZenBook14.probability += 0.2
+                asusZenBook15.probability += 0.2
+                asusZenBookS16.probability += 0.2
+                MacBookAir13.probability += 0.2
+                MacBookAir15.probability += 0.1
+                MacBookPro14.probability -= 10
+                MacBookPro16.probability -= 10
+                DellXPS13.probability += 0.2
+                DellXPS15.probability -= 10
+                DellXPS17.probability -= 10
             case 3:
-                budget = .large
-                recommendation += "Учитвая ваш бюджет, вам подойдут почти любые модели ноутбуков. "
+                break
             default: return
             }
         case 1:
             switch answerNumber {
             case 1:
-                switch budget {
-                case .small:
-                    recommendation += "Стоит присмотреться к Asus VivoBook, если вы собираетесь часто брать его с собой. "
-                case .medium:
-                    recommendation += "Стоит присмотреться к Dell XPS, если вы собираетесь часто брать его с собой. "
-                case .large:
-                    recommendation += "Стоит присмотреться к MacBook Air, если вы собираетесь часто брать его с собой. "
-                default: return
-                }
+                asusVivobook13.probability += 0.5
+                asusVivobook14.probability += 0.3
+                asusVivobook16.probability -= 10
+                asusZenBook14.probability += 0.3
+                asusZenBook15.probability += 0.2
+                asusZenBookS16.probability -= 10
+                MacBookAir13.probability += 0.5
+                MacBookAir15.probability += 0.2
+                MacBookPro14.probability += 0.2
+                MacBookPro16.probability -= 10
+                DellXPS13.probability += 0.5
+                DellXPS15.probability += 0.1
+                DellXPS17.probability -= 10
             case 2:
-                switch budget {
-                case .small:
-                    recommendation += "Стоит присмотреться к Asus ZenBook, так как для вас приоритетна мощность. "
-                case .medium:
-                    recommendation += "Стоит присмотреться к Asus ProArt, так как для вас приоритетна мощность. "
-                case .large:
-                    recommendation += "Стоит присмотреться к MacBook Pro, так как для вас приоритетна мощность. "
-                default: return
-                }
+                asusVivobook13.probability -= 0.2
+                asusVivobook14.probability -= 0.2
+                asusVivobook16.probability += 0.5
+                asusZenBook14.probability -= 0.2
+                asusZenBook15.probability += 0.2
+                asusZenBookS16.probability += 0.5
+                MacBookAir13.probability -= 0.3
+                MacBookAir15.probability -= 0
+                MacBookPro14.probability += 0
+                MacBookPro16.probability += 0.5
+                DellXPS13.probability -= 0.2
+                DellXPS15.probability += 0.2
+                DellXPS17.probability += 0.5
             case 3:
-                recommendation += "Можете рассмотреть ноутбук от Lenovo, для долговечности и без переплат. "
+                break
             default: return
             }
+            
         case 2:
             switch answerNumber {
             case 1:
-                recommendation += "Для долгой жизни вдали от розетки вам подойдёт MacBook. "
+                asusVivobook13.probability += 0.2
+                asusVivobook14.probability += 0.2
+                asusVivobook16.probability += 0.1
+                asusZenBook14.probability += 0.3
+                asusZenBook15.probability += 0.1
+                asusZenBookS16.probability -= 0.2
+                MacBookAir13.probability += 0.4
+                MacBookAir15.probability += 0.4
+                MacBookPro14.probability += 0.5
+                MacBookPro16.probability += 0.5
+                DellXPS13.probability += 0.2
+                DellXPS15.probability += 0.2
+                DellXPS17.probability -= 0.3
             case 2:
-                recommendation += ""
+                asusVivobook13.probability -= 0.2
+                asusVivobook14.probability -= 0.1
+                asusVivobook16.probability += 0.1
+                asusZenBook14.probability -= 0.2
+                asusZenBook15.probability += 0.2
+                asusZenBookS16.probability += 0.3
+                DellXPS13.probability -= 0.2
+                DellXPS15.probability += 0.2
+                DellXPS17.probability += 0.3
             case 3:
-                recommendation += "учитывая вашу неуверенность в работе от аккумулятора, можно попробовать ноутбук от HP. "
+                break
             default: return
             }
+            
         case 3:
             switch answerNumber {
             case 1:
-                switch budget {
-                case .small:
-                    recommendation += "Ваш наиболее приближенный вариант - Asus VivoBook 13."
-                case .medium:
-                    recommendation += "Ваш наиболее приближенный вариант - Dell XPS 13 или Asus Zenbook 13."
-                case .large:
-                    recommendation += "Ваш наиболее приближенный вариант - MacBook Air 13."
-                default: return
-                }
+                asusVivobook13.probability += 0.3
+                asusVivobook14.probability += 0.3
+                asusVivobook16.probability -= 10
+                asusZenBook14.probability += 0.4
+                asusZenBook15.probability -= 0.5
+                asusZenBookS16.probability -= 10
+                MacBookAir13.probability += 0.5
+                MacBookAir15.probability -= 0.5
+                MacBookPro14.probability += 0.1
+                MacBookPro16.probability -= 10
+                DellXPS13.probability += 0.4
+                DellXPS15.probability -= 0.5
+                DellXPS17.probability -= 10
             case 2:
-                switch budget {
-                case .small:
-                    recommendation += "Ваш наиболее приближенный вариант - Asus VivoBook 14."
-                case .medium:
-                    recommendation += "Ваш наиболее приближенный вариант - Dell XPS 15 или Asus Zenbook 14."
-                case .large:
-                    recommendation += "Ваш наиболее приближенный вариант - MacBook Air 15."
-                default: return
-                }
+                asusVivobook13.probability -= 0.5
+                asusVivobook14.probability -= 0.2
+                asusVivobook16.probability += 0.2
+                asusZenBook14.probability -= 0.2
+                asusZenBook15.probability += 0.5
+                asusZenBookS16.probability += 0.5
+                MacBookAir13.probability -= 0.5
+                MacBookAir15.probability += 0.5
+                MacBookPro14.probability -= 0.2
+                MacBookPro16.probability += 0.5
+                DellXPS13.probability -= 0.5
+                DellXPS15.probability += 0.5
+                DellXPS17.probability -= 10
             case 3:
-                switch budget {
-                case .small:
-                    recommendation += "Ваш наиболее приближенный вариант - Asus VivoBook 16X."
-                case .medium:
-                    recommendation += "Ваш наиболее приближенный вариант - Dell XPS 16 или Asus ProArt 16."
-                case .large:
-                    recommendation += "Ваш наиболее приближенный вариант - MacBook Air 15."
-                default: return
-                }
+                asusVivobook13.probability -= 10
+                asusVivobook14.probability -= 10
+                asusVivobook16.probability += 1
+                asusZenBook14.probability -= 10
+                asusZenBook15.probability -= 10
+                asusZenBookS16.probability += 0.3
+                MacBookAir13.probability -= 10
+                MacBookAir15.probability -= 10
+                MacBookPro14.probability -= 10
+                MacBookPro16.probability += 0.2
+                DellXPS13.probability -= 10
+                DellXPS15.probability -= 2
+                DellXPS17.probability += 2
             default: return
             }
         default: return
         }
+        
+        if currentQuestionIndex == questionsAndAnswers.count - 1 {
+            let laptopsArray: [Laptop] = [
+                asusVivobook13,
+                asusVivobook14,
+                asusVivobook16,
+                asusZenBook14,
+                asusZenBook15,
+                asusZenBookS16,
+                MacBookAir13,
+                MacBookAir15,
+                MacBookPro14,
+                MacBookPro16,
+                DellXPS13,
+                DellXPS15,
+                DellXPS17
+            ].sorted(by: { $0.probability > $1.probability})
+            
+            recommendation = """
+            - \(laptopsArray[0].name)
+            - \(laptopsArray[1].name)
+            - \(laptopsArray[2].name)
+            """
+        }
+        
+        
     }
     
     func restart() {
